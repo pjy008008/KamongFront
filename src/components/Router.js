2a9f9803e1505b55336bd4854d03bf26b721edb5
Junyou Park <pjy008008@gmail.com>
2024-01-01 18:44:25 +0900

    change the structure of router

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,54 +1,56 @@
 import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
+import Root from "./Root";
 import Home from "../routes/Home";
 import Admin from "../routes/Admin";
 import Select from "../routes/Select";
 import Detail from "../routes/Detail";
 import Script from "./admin/Script";
 
-const AppRouter = () => {
-  const router = createBrowserRouter([
-    {
-      path: `${process.env.PUBLIC_URL}/`,
-      element: (
-        <div>
-          <Home />
-        </div>
-      ),
-    },
-    {
-      path: `${process.env.PUBLIC_URL}/admin`,
-      element: (
-        <div>
-          <Admin />
-        </div>
-      ),
-    },
-    {
-      path: `${process.env.PUBLIC_URL}/select`,
-      element: (
-        <div>
-          <Select />
-        </div>
-      ),
-    },
-    {
-      //해당 페이지는 테스트 페이지로 실제로 routing하지는 않을거임
-      path: `${process.env.PUBLIC_URL}/test`,
-      element: (
-        <div>
-          <Detail />
-          {/* <Script /> */}
-        </div>
-      ),
-    },
-  ]);
-  return (
-    <div>
-      <RouterProvider router={router} />
-    </div>
-  );
-};
+const router = createBrowserRouter([
+  {
+    path: `${process.env.PUBLIC_URL}/`,
+    element: (
+      <div>
+        <Root />
+      </div>
+    ),
+    children: [
+      {
+        path: "",
+        element: (
+          <div>
+            <Home />
+          </div>
+        ),
+      },
+      {
+        path: "admin",
+        element: (
+          <div>
+            <Admin />
+          </div>
+        ),
+      },
+      {
+        path: "select",
+        element: (
+          <div>
+            <Select />
+          </div>
+        ),
+      },
+      {
+        path: "test",
+        element: (
+          <div>
+            <Detail />
+          </div>
+        ),
+      },
+    ],
+  },
+]);
 
-export default AppRouter;
+export default router;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./components/App";
+import { RouterProvider } from "react-router-dom";
+import router from "./components/Router";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(<RouterProvider router={router} />);
