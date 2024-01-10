0b99bce80750e69f71c989fdf957c17ffa95439e
JunyouPark <pjy008008@gmail.com>
2024-01-10 13:23:20 +0900

    update README.md

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 [Figma](https://www.figma.com/file/CihLl4WW3mxxRS03wQQAg1/%EC%B9%B4%EB%AA%BD%EC%9D%B4%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8?type=design&node-id=267-81&mode=design&t=66IRBF1XJEGvolEr-0)
 
+### To Do List
+- 체험 페이지 만들기(홈에서 체험 선택하고 다음페이지)
+- 체험 온오프 기능, 수정 기능 추가
+- 대사 전체 보기 기능 추가
--- src/components/select/Slide.js
+++ src/components/select/Slide.js
@@ -3,9 +3,9 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import styled from "styled-components";
-import axios from 'axios'
+import axios from "axios";
 
-const API_ENDPOINT = 'http://35.216.68.47:8080/api/experiences?page=0&size=50'; // Define API_ENDPOINT
+const API_ENDPOINT = "http://35.216.68.47:8080/api/experiences"; // Define API_ENDPOINT
 
 const CustomSlider = styled(Slider)`
   width: 80vw;
@@ -36,16 +36,15 @@ const BannerSlider = () => {
       try {
         const response = await axios.get(API_ENDPOINT, {
           headers: {
-            'Accept': '*/*'
-          }
+            Accept: "*/*",
+          },
         });
-        const responseData = response.data;
-        const contentArray = responseData.result.content;
+        const contentArray = response.data.result.content;
 
         // 전체 체험 정보를 상태 업데이트
         setExperiences(contentArray);
       } catch (error) {
-        console.error('에러:', error.message);
+        console.error("에러:", error.message);
       }
     };
 
