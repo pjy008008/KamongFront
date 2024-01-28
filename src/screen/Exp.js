ba29c5a7b801cd346af9e16ff18017580ef0ca8d
Seongae Kang <kk573113@gmail.com>
2024-01-28 23:49:15 +0900

    버튼

--- src/screen/Exp.js
+++ src/screen/Exp.js
@@ -3,6 +3,8 @@ import { useNavigate, useLocation } from "react-router-dom";
 import styled from "styled-components";
 import axios from "axios";
 import eraser from "../img/eraser.png";
+import pause from "../img/pause.png";
+import play from "../img/play.png";
 
 const StartContainer = styled.div`
   display: flex;
@@ -52,10 +54,25 @@ const Chalk2 = styled.div`
 const Button = styled.button`
   padding: 10px 20px;
   font-size: 16px;
+  font-weight: bold;
   background-color: white;
   color: black;
   border: none;
   cursor: pointer;
+  margin-top: 2vw;
+  margin-right: 0.8vw;
+  border-radius: 10px;
+`;
+
+const StartButton = styled.button`
+  padding: 10px 20px;
+  font-size: 30px;
+  font-weight: bold;
+  background-color: white;
+  color: black;
+  border: none;
+  cursor: pointer;
+  margin-top: 2vw;
   margin-right: 0.8vw;
   border-radius: 10px;
 `;
@@ -70,13 +87,35 @@ const GoBackButton = styled.button`
   cursor: pointer;
   margin-top: 20px;
   margin-right: auto;
+  margin-left: 1vw;
 `;
 
 const Title = styled.h2`
   color: white;
   font-weight: bold;
-  font-size: 30px;
+  font-size: 40px;
   margin: 0px;
+`;
+
+const CustomButton = styled.button`
+  padding: 0; /* padding을 0으로 설정하여 내부 여백을 없앱니다. */
+  border: none; /* 테두리를 없앱니다. */
+  background: none; /* 배경을 없앱니다. */
+  cursor: pointer;
+  position: relative;
+  margin-top: 1vw;
+  margin-left: 2vw;
+  margin-right: 2vw; /* 오른쪽 여백을 추가하여 다음 버튼과의 간격을 조절합니다. */
+`;
+
+const IconImg = styled.img`
+  width: 30px; /* 이미지의 크기에 맞게 조절하세요 */
+  height: 30px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  margin-top: 0.5vw;
 `;
 
 const Exp = () => {
@@ -85,6 +124,9 @@ const Exp = () => {
   const navigate = useNavigate();
   const [steps, setSteps] = useState([]);
   const [count, setCount] = useState(0);
+  const [audioPlayer, setAudioPlayer] = useState(null); // 상태 추가
+  const [isPlaying, setIsPlaying] = useState(false); // 재생 상태 추가
+
   useEffect(() => {
     axios
       .get(`http://35.216.68.47:8080/api/experiences/${expId}/pages`)
@@ -112,12 +154,25 @@ const Exp = () => {
   const goBack = () => {
     navigate("/select");
   };
+
+  const toggleAudio = (url) => {
+    if (audioPlayer && isPlaying) {
+      audioPlayer.pause(); // 재생 중인 오디오를 멈춤
+      setIsPlaying(false); // 재생 상태 업데이트
+    } else {
+      const audio = new Audio(url);
+      audio.play(); // 오디오 재생
+      setAudioPlayer(audio); // 오디오 플레이어 설정
+      setIsPlaying(true); // 재생 상태 업데이트
+    }
+  };
+
   const handleExp = (sequence) => {
     if (sequence == 0) {
       return (
         <Blackboard>
           <Title>카몽이와 함께 하는 {expTitle}</Title>
-          <Button onClick={nextBtn}>체험 시작</Button>
+          <StartButton onClick={nextBtn}>체험 시작</StartButton>
         </Blackboard>
       );
     } else if (sequence - 1 == steps.length) {
@@ -151,19 +206,15 @@ const Exp = () => {
             }}
           >
             <Button onClick={prevBtn}>이전</Button>
-            <audio
-              controls
-              key={steps[sequence - 1].voiceUrl}
-              style={{
-                paddingBottom: "0.1vw",
-              }}
+            <CustomButton
+              onClick={() => toggleAudio(steps[sequence - 1].voiceUrl)}
             >
-              <source src={steps[sequence - 1].voiceUrl} type="audio/mpeg" />
-              Your browser does not support the audio tag.
-            </audio>
-            <Button style={{ marginLeft: "10px" }} onClick={nextBtn}>
-              다음
-            </Button>
+              <IconImg
+                src={isPlaying ? pause : play}
+                alt={isPlaying ? "멈춤" : "재생"}
+              />
+            </CustomButton>
+            <Button onClick={nextBtn}>다음</Button>
           </div>
 
           <Title>{steps[sequence - 1].title}</Title>
@@ -172,7 +223,7 @@ const Exp = () => {
           </p>
           <p>
             <img
-              style={{ width: "700px", height: "380px" }}
+              style={{ width: "900px", height: "480px" }}
               src={steps[sequence - 1].imageUrl}
               alt="Experience Image"
             />
